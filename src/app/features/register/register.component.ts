64f7b40188e638fe092bb00c324da2b7fd38e71f
alfarouk <aowaleed99@gmail.com>
2025-08-26 19:50:41 +0300

    register page

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,34 +1,70 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
+
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms"
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule , CommonModule],
+  imports: [   ReactiveFormsModule  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
-  // !!! form 
+ 
+  // !!! reactive forms (FormGroup , FormControl , formArray , Validators , formBuilder , formControlName) ==> ReactiveFormsModule
+ //! form in ts
+ //! form UI 
+ //! form tag ==> [formGroup] ==> registerForm
+ //! input formControlName=""
 
-  registerForm = new FormGroup({
-    name : new FormControl ( 'ahmed'  ,   [Validators.required ,Validators.minLength(7)] ),
-    email : new FormControl(null ,[Validators.required , Validators.email]) ,
-    password : new FormControl(null ) ,
-    rePassword : new FormControl(null ) ,
-    phone : new FormControl(null  ) ,
+//  !!! custom validation check rePassword
+
+  registerForm : FormGroup = new FormGroup({
+    name : new FormControl('' , [Validators.required , Validators.minLength(3) , Validators.maxLength(10) ]), 
+    email : new FormControl('' , [Validators.required , Validators.email ]),
+    phone : new FormControl('' , [Validators.required , Validators.pattern(/^01[0125][0-9]{8}$/)]),
+
+
+
+
+    password : new FormControl('' , [Validators.required , Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)]),
+    rePassword : new FormControl('' , [Validators.required , Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)] ),
+
   });
 
-
+//   
   handleSubmit(){
-    //  null
-    console.log({nameErrors : this.registerForm.get('name')?.errors , emailErrors : this.registerForm.get('email')?.errors });
-    
-    console.log("register :-" , this.registerForm.value);
+
+
+    // !!!
+    if(this.registerForm.invalid){
+    this.registerForm.markAllAsTouched();
+      return;
+    }
+    // !!!!
+    console.log(this.registerForm.value);
+    // !!
+  
     
   }
 
+
+  get nameController(){
+    return this.registerForm.get('name')
+  }
+  get emailController(){
+    return this.registerForm.get('email')
+  }
+  get passwordController(){
+    return this.registerForm.get('password')
+  }
+  get rePasswordController(){
+    return this.registerForm.get('rePassword')
+  }
+  get phoneController(){
+    return this.registerForm.get('phone')
+  }
 
 
 
